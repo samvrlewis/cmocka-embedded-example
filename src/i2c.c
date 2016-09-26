7843a51b01a3969427dc10cdb8ce3885f57904cd
Sam Lewis <samvrlewis@users.noreply.github.com>
2016-09-26 16:45:31 +1000

    Update i2c.c

--- src/i2c.c
+++ src/i2c.c
@@ -5,14 +5,17 @@
  */
 void i2c_transmit_blocking(uint8_t address, uint8_t offset, uint8_t* data, uint8_t data_size)
 {
-    // implementation todo later
+    // normally there would be an implementation of this function but as this project
+    // is only to show how to use cmocka to write tests, an implementation of this function
+    // is not needed as it is mocked out by the tests
 }
-
 
 /*
  * Reads data into pData from the specified i2c address in blocking mode
  */
 void i2c_read_blocking(uint8_t address, uint8_t offset, uint8_t* pData, uint8_t data_size)
 {
-    // implementation to do later
+    // normally there would be an implementation of this function but as this project
+    // is only to show how to use cmocka to write tests, an implementation of this function
+    // is not needed as it is mocked out by the tests
 }
