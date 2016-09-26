af095b1b53c7b19e16896e8a9927614f072f5071
Sam Lewis <sam.vr.lewis@gmail.com>
2016-09-26 12:07:33 +1000

    add some more tests

--- test/mock_i2c.c
+++ test/mock_i2c.c
@@ -7,16 +7,16 @@
 
 void __wrap_i2c_transmit_blocking(uint8_t address, uint8_t offset, uint8_t* data, uint8_t data_size)
 {
-
-
-
+    // allows the calling test to check if the supplied parameters are as expected
+    check_expected(address);
+    check_expected(offset);
 }
 
 void __wrap_i2c_read_blocking(uint8_t address, uint8_t offset, uint8_t* pData, uint8_t data_size) 
 {
-	// allow the calling test to specify the data it wants back
-	// and copy it back out
-	for(int i=0; i < data_size; i++) {
-		pData[i] = mock_type(uint8_t);
-	}
+    // allow the calling test to specify the data it wants back
+    // and copy it back out
+    for(int i=0; i < data_size; i++) {
+        pData[i] = mock_type(uint8_t);
+    }
 }
--- test/test_tmp101.c
+++ test/test_tmp101.c
@@ -6,27 +6,59 @@
 
 #include "tmp101.h"
 
-static void test_positive_temperature(void **state)
+static void check_i2c_transmit(void)
 {
-     will_return(__wrap_i2c_read_blocking, 0b11001001);
-     will_return(__wrap_i2c_read_blocking, 0b0000000);
-
-     float test = tmp101_get_temperature();
+    expect_value(__wrap_i2c_transmit_blocking, address, 0x48);
+    expect_value(__wrap_i2c_transmit_blocking, offset, 0);
 }
 
 static void test_negative_temperature(void **state)
 {
-	 will_return(__wrap_i2c_read_blocking, 0b01001011);
-     will_return(__wrap_i2c_read_blocking, 0b00000000);
+    will_return(__wrap_i2c_read_blocking, 0b11100111);
+    will_return(__wrap_i2c_read_blocking, 0b00000000);
+    
+    check_i2c_transmit();
+    
+    assert_true(tmp101_get_temperature() == -25);
+}
 
-     assert_in_range(tmp101_get_temperature(), 78.9, 79.5);
+static void test_zero_temperature(void **state)
+{
+    will_return(__wrap_i2c_read_blocking, 0b00000000);
+    will_return(__wrap_i2c_read_blocking, 0b00000000);
+    
+    check_i2c_transmit();
+
+    assert_true(tmp101_get_temperature() == 0);
+}
+
+static void test_positive_temperature(void **state)
+{
+    will_return(__wrap_i2c_read_blocking, 0b01001011);
+    will_return(__wrap_i2c_read_blocking, 0b00000000);
+    
+    check_i2c_transmit();
+
+    assert_true(tmp101_get_temperature() == 75);
+}
+
+static void test_fractional_temperature(void **state)
+{
+    will_return(__wrap_i2c_read_blocking, 0b00000000);
+    will_return(__wrap_i2c_read_blocking, 0b01000000);
+    
+    check_i2c_transmit();
+
+    assert_true(tmp101_get_temperature() == 0.25);
 }
 
 int main(void)
 {
 	const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_positive_temperature),
-        cmocka_unit_test(test_negative_temperature)
+        cmocka_unit_test(test_negative_temperature),
+        cmocka_unit_test(test_zero_temperature),
+        cmocka_unit_test(test_fractional_temperature)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
